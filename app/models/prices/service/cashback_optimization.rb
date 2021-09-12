0555b027b21a0184fd19d7a2c433f782467ed96b
Jeroen Huisman <jphuisman92@gmail.com>
2021-09-12 13:26:35 +0200

    Fix cashback optimization

--- app/models/prices/service/cashback_optimization.rb
+++ app/models/prices/service/cashback_optimization.rb
@@ -4,21 +4,25 @@ module Prices
       SUPPORTED_AMOUNTS = [50, 20, 10, 5, 2, 1, 0.5, 0.2]
 
       def initialize(amount)
-        @amount = Prices::Service::RoundPrice.new(amount).call
+        @initial = Prices::Service::RoundPrice.new(amount).call
+        @amount = @initial
       end
 
       def call
-        cashbacks.to_h
+        {
+          cashback: @initial,
+          amounts: amounts.to_h
+        }
       end
 
       private
 
       attr_reader :amount
 
-      def cashbacks
+      def amounts
         SUPPORTED_AMOUNTS.map do |value|
           times = (amount / value).floor
-          @amount = amount - (times * value)
+          @amount = (amount - (times * value)).round(2)
           [value, times]
         end
       end
