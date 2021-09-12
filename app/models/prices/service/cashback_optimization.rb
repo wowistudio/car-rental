1090ba6c1a239fffe4908bd6720fe7e5f99ab6dd
Jeroen Huisman <jphuisman92@gmail.com>
2021-09-12 09:27:33 +0200

    Smarten cashback optimization & round price

--- app/models/prices/service/cashback_optimization.rb
+++ app/models/prices/service/cashback_optimization.rb
@@ -6,14 +6,22 @@ module Prices
       end
 
       def call
-        amount
+        cashbacks.to_h
       end
 
       private
 
       attr_reader :amount
 
-      def supported
+      def cashbacks
+        supported_values.map do |value|
+          times = (amount / value).floor
+          @amount = amount - (times * value)
+          [value, times]
+        end
+      end
+
+      def supported_values
         [50, 20, 10, 5, 2, 1, 0.5, 0.2]
       end
     end
--- app/models/prices/service/round_price.rb
+++ app/models/prices/service/round_price.rb
@@ -6,12 +6,36 @@ module Prices
       end
 
       def call
-        price.round(2)
+        price.floor + rounded_remainder
       end
 
       private
 
       attr_reader :price
+
+      def rounded_remainder
+        roundup_map
+          .select { |key, _| key[:min] <= remainder && key[:max] >= remainder }
+          .values[0]
+          .to_f
+      end
+
+      def remainder
+        (price - price.floor).round(2)
+      end
+
+      def roundup_map
+        {
+          { min: 0.01, max: 0.20 } => 0.20,
+          { min: 0.21, max: 0.40 } => 0.40,
+          { min: 0.41, max: 0.50 } => 0.50,
+          { min: 0.51, max: 0.60 } => 0.60,
+          { min: 0.61, max: 0.70 } => 0.70,
+          { min: 0.71, max: 0.80 } => 0.80,
+          { min: 0.81, max: 0.90 } => 0.90,
+          { min: 0.91, max: 0.99 } => 1.00
+        }
+      end
     end
   end
 end
